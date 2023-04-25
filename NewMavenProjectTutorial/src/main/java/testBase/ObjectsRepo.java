3eb46fd17f9ba1a701d300b2597c30a88779fff2
agustinj <agustin.jauregui2@gmail.com>
2023-04-25 11:27:37 +0200

    Fixed Object call in ObjectsRepo

--- NewMavenProjectTutorial/src/main/java/testBase/ObjectsRepo.java
+++ NewMavenProjectTutorial/src/main/java/testBase/ObjectsRepo.java
@@ -1,15 +1,15 @@
 package testBase;
 
+import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 import pageObject.EnterPracticeFormDataObjects;
 import pageObject.HomePageObjects;
-import reusableComponents.ExtentSetup;
 
 public class ObjectsRepo {
 
     public static WebDriver driver;
-    public static ExtentSetup extent;
+    public static ExtentReports extent;
     public static ExtentTest test;
     public static HomePageObjects homepage;
     public EnterPracticeFormDataObjects practiceform;
